c6346701d2a4ee3a7b82840dbfa3ec50b0fb9ec4
Laclcr <laclcr@floridauniversitaria.es>
2023-01-13 15:41:40 +0100

    Operació 1

---
+++ op1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 
+read -p "Introdueix un nom d'usuari:" nomUsuari
+
+apa=`cat usuarios.txt | grep $nomUsuari | wc -l`
+
+if [ $apa -eq 0 ]
+    then
+        echo "El nom d'usuari introduït ($nomUsuari) no existeix."
+    else
+        echo "L'usuari $nomUsuari s'ha loguejat $apa vegades."
+    fi
